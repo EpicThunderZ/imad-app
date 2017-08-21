20f69c68d76de50e2f68074fcd9b6464a2d2582f
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-08-21 10:12:31 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+/*
 console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "New value";
@@ -12,3 +13,15 @@ function moveRight () {
 img.onclick = function () {
     var interval = setInterval(moveRight, 50);
 };
+*/
+//Counter Code
+var button = document.getElementById("Counter");
+var counter=0;
+button.onclick = function () {
+    //Make a request to the counter endpoint
+    // Capture the respones and store it in a variaable
+    //Render the variable in the correct span
+    counterr++;
+    var span=document.getElementById("Count");
+    span.innerHTML = counter.toString();
+}
