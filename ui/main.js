2e971f47c6a0353b9a0cbc52988a61138faab348
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-08-21 08:23:18 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,7 @@ element.innerHTML = "New value";
 //Move the image
 var img =document.getElementById("madi");
 function moveRight () {
+    var marginLeft = 0;
     marginLeft-=10;
     img.style.marginleft = marginLeft+"px";
 }
