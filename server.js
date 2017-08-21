731621a0ef7e91b43a764ed07b2bcb3e3df0338e
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-08-21 09:58:20 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,6 +69,11 @@ return HTMLtemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+}); 
+
+app.get('/counter', function (req,res) {
+    counter++;
+    res.send(counter.toString());
 });
 
 app.get('/:articleName',function (req, res) {
