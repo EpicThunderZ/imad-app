a0b5c236b92e6fbd68787a462a95cb547b5b6850
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-08-21 08:26:15 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,7 @@ var marginLeft = 0;
 function moveRight () {
     marginLeft-=10;
     img.style.marginleft = marginLeft+"px";
+    console.log("done");
 }
 img.onclick = function () {
     var interval = setInterval(moveRight, 100);
