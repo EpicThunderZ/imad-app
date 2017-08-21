d147002dace46d7f84fe8813a0cd62cec937ff89
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-08-21 19:17:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,55 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var ArticleOne = {
+    title: 'Article One | Janak Shah',
+    heading: 'Article One',
+    date: 'Aug 21 2017',
+    content: `<p>This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.</p>
+            <p>This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.</p>
+            <p>This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.</p>`
+};
+function createTemplate (data) {
+    var title=data.title;
+    var date= data.date;
+    var heading = data.heading;
+    var content = data.content;
+
+var HTMLtemplate= 
+`
+<html>
+    <head>
+        <title>
+            ${title}
+            
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet"/>
+        
+    </head>
+    <body>
+        <div class="container">
+        <div>
+           <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+           ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createTemplate{ArticleOne});
 });
 
 app.get('/article-one',function (req, res) {
