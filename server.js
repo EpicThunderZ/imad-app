a93c89a2450099cee823190fd577f0178d6d1bbb
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-08-21 19:22:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var HTMLtemplate=
 <html>
     <head>
         <title>
-            ${title}
+        ${title}
             
         </title>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
