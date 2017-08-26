b60af2d05313fc6ab587c85a4afaa3b835399e57
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-08-26 06:55:02 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,11 +16,25 @@ img.onclick = function () {
 */
 //Counter Code
 var button = document.getElementById("counter");
-var counter=0;
+
 button.onclick = function() {
-    counter++;
-    var span=document.getElementById("count");
-    span.innerHTML = counter.toString();
+    //Create a request objeact
+    var request = new XMLHttpRequest();
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readystate === XMLHttpRequest.DONE) {
+            if(request.status===200) {
+                var counter = request.responseText;
+                var span=document.getElementById("count");
+                span.innerHTML = counter.toString();
+
+            }
+        }
+        
+    }
     
+    //Make the request
+    request.open('GET', 'http://janak31415et.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
     
