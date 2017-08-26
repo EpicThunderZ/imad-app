18e33ddd798edec1b66320949127ed064a904183
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-08-26 07:02:34 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,4 @@
-/*
-console.log('Loaded!');
-var element = document.getElementById("main-text");
-element.innerHTML = "New value";
-//Move the image
-var img =document.getElementById("madi");
-var marginLeft = 0;
-function moveRight () {
-    marginLeft+=2;
-    img.style.marginLeft = marginLeft+"px";
-    console.log("done");
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-};
-*/
-//Counter Code
+
 var button = document.getElementById("counter");
 
 button.onclick = function() {
@@ -25,7 +9,7 @@ button.onclick = function() {
         if(request.readystate === XMLHttpRequest.DONE) {
             if(request.status===200) {
                 var counter = request.responseText;
-                var span=document.getElementById("count");
+                var span=document.getElementById('count');
                 span.innerHTML = counter.toString();
 
             }
