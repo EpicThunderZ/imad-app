d01fcea117003e981499bf16019fa4ea9531e641
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-08-26 11:01:01 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,10 +95,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names=[];
-app.get('/submit-name/:name', function(req,res) {
+app.get('/submit-name', function(req,res) {
     //Get the name for the request
 
-    var name = req.params.name;
+    var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
