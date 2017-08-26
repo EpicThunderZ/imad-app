cebaa031bdd43b119e2ff211e364c6a4d059ad76
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-08-26 11:09:32 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,10 +30,17 @@ var nameInput = document.getElementById("name");
 var name = nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick = function () {
-    //Make a request to the server and send the name
-    
-    //Capture a list of names and render it as a list
-    var names=["name1","name1","name3","name4"];
+    //Create a request object
+    var request = new XMLHttpRequest();
+    console.log('clicked');
+
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status === 200) {
+                //Capture a list of names and render it as a list
+    var names= JSON.parse(names);
     var list = "";
     for(var i=0; i<names.length; i++) {
         list+='<li>'+names[i] + '</li>';
@@ -42,5 +49,16 @@ submit.onclick = function () {
     }
     var ul = document.getElementById("namelist");
     ul.innerHTML=list;
+            }
+        }
+        //Not Done Yet
+    };
+    
+    //Make the request
+    request.open('GET', 'http://janak31415et.imad.hasura-app.io/submit-name?name='+name, true);
+    request.send(null);
+    //Make a request to the server and send the name
+    
+    
 
     };
