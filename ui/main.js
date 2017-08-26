46fe0083ca9a8bd2a78b5a4b8385dde960ab902d
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-08-26 07:20:55 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,8 +10,11 @@ button.onclick = function() {
     //Capture the response and store it in a variable
     request.onreadystatechange = function() {
         if(request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
             if(request.status === 200) {
+                
                 var counter = request.responseText;
+                console.log(counter);
                 var span=document.getElementById('count');
                 span.innerHTML = counter.toString();
             }
