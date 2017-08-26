f82c9c2284cfb3518ccf6edc2675489322492eee
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-08-26 11:14:14 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,7 @@ button.onclick = function() {
 };
 
 //Submit Name
-var nameInput = document.getElementById("name");
-var name = nameInput.value;
+
 var submit = document.getElementById("submit_btn");
 submit.onclick = function () {
     //Create a request object
@@ -55,6 +54,8 @@ submit.onclick = function () {
     };
     
     //Make the request
+    var nameInput = document.getElementById("name");
+var name = nameInput.value;
     request.open('GET', 'http://janak31415et.imad.hasura-app.io/submit-name?name='+name, true);
     request.send(null);
     //Make a request to the server and send the name
