486bc0f7a6d923be6e5a9cad655d718a3d2e7997
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-09-09 00:10:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+/*
 //Counter code
 var button = document.getElementById('counter');
 button.onclick = function() {
@@ -25,8 +26,8 @@ button.onclick = function() {
     request.send(null);
 };
 
-//Submit Name
 
+//Submit Name
 var submit = document.getElementById("submit_btn");
 submit.onclick = function () {
     //Create a request object
@@ -59,6 +60,42 @@ submit.onclick = function () {
 var name = nameInput.value;
     request.open('GET', 'http://janak31415et.imad.hasura-app.io/submit-name?name='+name, true);
     request.send(null);
+    //Make a request to the server and send the name
+    
+    
+
+    };
+*/
+//Submit username/password to login
+var submit = document.getElementById("submit_btn");
+submit.onclick = function () {
+    //Create a request object
+    var request = new XMLHttpRequest();
+    console.log('clicked');
+
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status === 200) {
+               console.log('User logged on.');
+               alert('Logged in successfully.');
+            } else if(request.status === 403) {
+                alert('Username/password is incorrect.');
+            } else if(request.status === 500) {
+                alert('Something went wrong on the server.');
+            }
+        }
+        //Not Done Yet
+    };
+    
+    //Make the request
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://janak31415et.imad.hasura-app.io/login', true);
+    request.send(JSON.stringify({username: username, password: password }));
     //Make a request to the server and send the name
     
     
