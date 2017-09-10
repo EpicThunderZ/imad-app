910d750bbe24c3e3756ed9d6c289d863dfa29caf
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-09-10 22:08:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,6 +132,11 @@ app.get('/check-login', function(req, res) {
     }
     
 });
+
+app.get('/logout', function(req, res) {
+    delete req.session.auth; 
+    res.send('You have logged out');
+});
 var pool = new Pool(config);
 app.get('/test-db', function(req,res) {
     pool.query('SELECT * FROM test', function(err, result) {
