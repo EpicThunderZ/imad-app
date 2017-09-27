09ca8dcffed7bc4be4c6376f7a0d476545518346
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-09-27 13:15:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -278,7 +278,7 @@ app.listen(port, function () {
   app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 }); 
-  var port = 8080;
+  var port = 80;
   app.listen(port, function () {
     console.log(`App listening on port ${port}!`);
   });
