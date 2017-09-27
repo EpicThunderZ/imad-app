912510d170a306731259779dbd9bcb9fd07041dc
EpicThunderZ <EpicThunderZ@users.noreply.github.com>
2017-09-27 13:14:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
@@ -211,3 +212,74 @@ var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+*/
+  //Now my code starts
+  var express = require('express');
+  var morgan = require('morgan');
+  var path = require('path');
+  
+
+
+  var app = express();
+  app.use(morgan('combined'));
+  
+
+  var articles= {
+    'article-one': {
+      heading:'Planets',
+      content:'Planets'
+    },
+    'article-two': {
+      heading:'Planets',
+      content:'Planets'
+    },
+    'article-three': {
+      heading:'Planets',
+      content:'Planets'
+    },
+    'article-four': {
+      heading:'Planets',
+      content:'Planets'
+    }
+  };
+
+  function createTemplate(data) {
+  var heading= data.heading;
+  var content= data.content;
+  var HTMLtemplate=
+  `
+  <html>
+      <head>
+
+          <link href="/ui/style.css" rel="stylesheet"/>
+
+      </head>
+      <body>
+          <div class="container">
+          <div>
+             <a href="/">Home</a>
+          </div>
+          <hr/>
+          <h3>
+              ${heading}
+          </h3>
+          <div>
+              ${content}
+          </div>
+          </div>
+      </body>
+  </html>
+  `;
+  return HTMLtemplate;
+  }
+  app.get('/:articleName', function(res, req) {
+    res.send(createTemplate(articles(articleName)));
+  });
+  app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+}); 
+  var port = 8080;
+  app.listen(port, function () {
+    console.log(`App listening on port ${port}!`);
+  });
+
