cf4f277710e70fd5c10b9a677d8bc5e994508832
Janak Shah <EpicThunderZ@users.noreply.github.com>
2017-11-21 15:35:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -138,6 +138,7 @@ function loadLoggedInUser (username) {
     `;
 }
 function loadLogin () {
+    console.log("Loading login status")
     // Check if the user is already logged in
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
