4b2dbba5952c8e4341a09bb7595c0b1d0d2834cc
Janak Shah <janak3.1415et@gmail.com>
2018-03-26 22:04:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.post('/create-user', function(req, res) {
     var password = req.body.password;
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString=hash(password, salt);
-    pool.query("INSERT INTO `user`(`username`,`password`) VALUES ('"+req.body.username+"','"+dbString+"')", function(err, field, result) {
+    pool.query('INSERT into "user" (username, password) VALUES ($1,$2)', [username, dbString], function(err, result) {
         if(err) {
             res.status(500).send(err.toString());
         }
@@ -92,26 +92,30 @@ app.post('/create-user', function(req, res) {
 });
 
 app.post('/login', function(req, res) {
+
     var username = req.body.username;
     var password = req.body.password;
-    pool.query("SELECT * FROM `user` WHERE `username` = '"+req.body.username+"' ", function(err, result, field) {
+    pool.query('SELECT * from "user" WHERE username = $1', [username], function(err, result) {
         if(err) {
             res.status(500).send(err.toString());
-        } else {
-            if(result.length === 0) {
-                res.status(400).send('Username/Password is invalid');
+        }
+        else {
+            if(result.rows.length === 0) {
+                res.status(403).send('Username/Password is invalid');
             }
             else{
-                 var dbString = result[0].password;
+                 var dbString = result.rows[0].password;
                  var salt = dbString.split('$')[2];
                  var hashedPassword = hash(password, salt);
                  if(hashedPassword === dbString) {
-                     req.session.auth = {userId: result[0].username};
+                     req.session.auth = {userId: result.rows[0].id};
+
                      res.send('Credentials correct');
-                   } else{
-                     res.status(403).send('Username/Password is invalid');
-                   }
                  }
+                 else{
+                     res.status(403).send('Username/Password is invalid');
+                 }
+            }
         }
 
     });
@@ -121,27 +125,27 @@ app.get('/check-login', function (req, res) {
        var id=req.session.auth.userId;
        res.send("You are logged in: "+req.session.auth.userId);
    } else {
-       res.status(400).send('You are not logged in.');
+       res.status(400).send('You are not logged in');
    }
 });
 
 app.get('/logout', function(req, res) {
     delete req.session.auth;
-    res.send('You have logged out.<br><a href="/">Home</a>');
+    res.send('You have logged out');
 });
 
 
-//var pool = new Pool(config);
+var pool = new Pool(config);
 app.get('/test-db', function(req,res) {
-  var usertest='EpicThunder';
-  pool.query("SELECT * FROM `user` WHERE `username`='"+usertest+"'", function (err, result, fields) {
-    if(err) {
-      res.send('error');
-    }
-    res.send(result);
-  });
+    pool.query('SELECT * FROM test', function(err, result) {
+        if(err) {
+            res.status(500).send(err.toString());
+        }
+        else {
+            res.send(JSON.stringify(result.rows));
+        }
+    });
 });
-
 var counter=0;
 app.get('/counter', function (req,res) {
     counter = counter + 1;
@@ -159,91 +163,32 @@ app.get('/submit-name', function(req,res) {
     res.send(JSON.stringify(names));
 });
 
-/*
-app.get('/get-comments/:programName', function (req, res) {
-   // make a select request
-   // return a response with the results
-   pool.query('SELECT comment.*, "user".username FROM article, comment, "user" WHERE article.title = ? AND article.id = comment.article_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', req.params.articleName, function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          res.send(JSON.stringify(result.rows));
-      }
-   });
-});
-
-app.post('/submit-comment/:programName', function (req, res) {
-   // Check if the user is logged in
-    if (req.session && req.session.auth && req.session.auth.userId) {
-        // First check if the article exists and get the article-id
-        pool.query('SELECT * from programs where tag = ?', [req.params.articleName], function (err, result) {
-            if (err) {
-                res.status(500).send(err.toString());
-            } else {
-                if (result.rows.length === 0) {
-                    res.status(400).send('Article not found');
-                } else {
-                    var program = result[0].heading;
-                    // Now insert the right comment for this article
-                    pool.query(
-                        "INSERT INTO ProgComments (comment, program, user) VALUES (?, ?, ?)",
-                        [req.body.comment, articleId, req.session.auth.userId],
-                        function (err, result) {
-                            if (err) {
-                                res.status(500).send(err.toString());
-                            } else {
-                                res.status(200).send('Comment inserted!')
-                            }
-                        });
-                }
-            }
-       });     
-    } else {
-        res.status(403).send('Only logged in users can comment');
-    }
-});
-*/
-
-
 app.get('/articles/:ArticleName',function (req, res) {
-  console.log(req.params.ArticleName);
-    pool.query("SELECT * FROM article WHERE title = ?",[req.params.ArticleName], function(err, result) {
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.ArticleName], function(err, result) {
         if(err) {
             res.status(500).send(err.toString());
         }
         else{
-            if(result.length === 0) {
+            if(result.rows.length === 0) {
                 res.status(404).send('Article not found');
             }
             else {
-                var articleData= result[0];
+                var articleData= result.rows[0];
                 res.send(createTemplate(articleData));
             }
         }
     });
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
-pool.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
 
 var port = 8080;
 app.listen(port, function () {
-  console.log(`App listening on port ${port}!`);
+  console.log(`IMAD course app listening on port ${port}!`);
 });
