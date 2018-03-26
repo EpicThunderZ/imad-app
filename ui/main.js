8a0277a53e15863d63efff3d7a583732ac1237d4
Janak Shah <janak3.1415et@gmail.com>
2018-03-26 21:56:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,96 +1,26 @@
-/*
-//Counter code
-var button = document.getElementById('counter');
-button.onclick = function() {
-    
-    //Create a request object
-    var request = new XMLHttpRequest();
-    console.log('clicked');
-
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            //Take some action
-            if(request.status === 200) {
-                var counter = request.responseText;
-                console.log(counter);
-                var span=document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-        //Not Done Yet
-    };
-    
-    //Make the request
-    request.open('GET', 'http://janak31415et.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
-
-
-//Submit Name
-var submit = document.getElementById("submit_btn");
-submit.onclick = function () {
-    //Create a request object
-    var request = new XMLHttpRequest();
-    console.log('clicked');
-
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            //Take some action
-            if(request.status === 200) {
-                //Capture a list of names and render it as a list
-    var names = request.responseText;
-    names= JSON.parse(names);
-    var list = "";
-    for(var i=0; i<names.length; i++) {
-        list+='<li>'+names[i] + '</li>';
-    
-
-    }
-    var ul = document.getElementById("namelist");
-    ul.innerHTML=list;
-            }
-        }
-        //Not Done Yet
-    };
-    
-    //Make the request
-    var nameInput = document.getElementById("name");
-var name = nameInput.value;
-    request.open('GET', 'http://janak31415et.imad.hasura-app.io/submit-name?name='+name, true);
-    request.send(null);
-    //Make a request to the server and send the name
-    
-    
-
-    };
-*/
-function loadLoginForm() {
+function loadLoginForm () {
     var loginHtml = `
         <h3>Login/Register to unlock awesome features</h3>
-        <input type="text" id="username" placeholder="Username" />
-        <input type="password" id="password" placeholder="Password"/>
+        <input type="text" id="username" placeholder="username" />
+        <input type="password" id="password" />
         <br/><br/>
-        <input type="submit" id="submit_btn" value="Login" />
+        <input type="submit" id="login_btn" value="Login" />
         <input type="submit" id="register_btn" value="Register" />
         `;
     document.getElementById('login_area').innerHTML = loginHtml;
-
-
-//Submit username/password to login
-var submit = document.getElementById("submit_btn");
-submit.onclick = function () {
-    //Create a request object
-    var request = new XMLHttpRequest();
-    console.log('clicked');
-
-    //Capture the response and store it in a variable
-     request.onreadystatechange = function () {
+    
+    // Submit username/password to login
+    var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
-                  submit.value = 'Success!';
+                  submit.value = 'Sucess!';
               } else if (request.status === 403) {
                   submit.value = 'Invalid credentials. Try again?';
               } else if (request.status === 500) {
@@ -100,12 +30,13 @@ submit.onclick = function () {
                   alert('Something went wrong on the server');
                   submit.value = 'Login';
               }
-            loadLogin();
-        }
-        //Not Done Yet
-    };
-    
-    //Make the request     var username = document.getElementById('username').value;
+              loadLogin();
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
         console.log(username);
         console.log(password);
@@ -113,21 +44,17 @@ submit.onclick = function () {
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
         submit.value = 'Logging in...';
-    //Make a request to the server and send the name
-    
-
+        
     };
-
     
-var reg = document.getElementById("register_btn");
-reg.onclick = function () {
-    //Create a request object
-    var request = new XMLHttpRequest();
-    console.log('clicked reg');
-
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function() {
-         if (request.readyState === XMLHttpRequest.DONE) {
+    var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
                   alert('User created successfully');
@@ -137,11 +64,10 @@ reg.onclick = function () {
                   register.value = 'Register';
               }
           }
-        //Not Done Yet
-    };
-    
-    //Make the request
-    var username = document.getElementById('username').value;
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
         console.log(username);
         console.log(password);
@@ -150,10 +76,8 @@ reg.onclick = function () {
         request.send(JSON.stringify({username: username, password: password}));  
         register.value = 'Registering...';
     
-    };    
-
+    };
 }
-
 
 function loadLoggedInUser (username) {
     var loginArea = document.getElementById('login_area');
@@ -179,4 +103,36 @@ function loadLogin () {
     request.open('GET', '/check-login', true);
     request.send(null);
 }
+
+function loadArticles () {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
+            if (request.status === 200) {
+                var content = '<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li>
+                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                    (${articleData[i].date.split('T')[0]})</li>`;
+                }
+                content += "</ul>"
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!')
+            }
+        }
+    };
+    
+    request.open('GET', '/get-articles', true);
+    request.send(null);
+}
+
+
+// The first thing to do is to check if the user is logged in!
 loadLogin();
+
+// Now this is something that we could have directly done on the server-side using templating too!
+loadArticles();
